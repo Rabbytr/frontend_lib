5d2ee6dd6cb97e8f14a4cc54850a4e1e81cf8fa8
Rabbytr <972592943@qq.com>
2018-05-14 01:07:27 +0800

    improve the effect of mousetext

--- events/mouseeffects.js
+++ events/mouseeffects.js
@@ -1,12 +1,27 @@
-word24 = function(dom){
+mouseword = function(dom,texts
+   = ['富强','民主','文明','和谐','自由','平等',
+  '公正','法治','爱国','敬业','诚信','友善']){
+    //two params,the first is the dom you wanted to effect
+    //the second one is the texts that you wanted to show
   $(dom).mousedown(function(event){
-    text = ['富强','民主','文明','和谐','自由','平等','公正','法治','爱国','敬业','诚信','友善'];
     x = event.clientX;
     y = event.clientY;
-    ind = Math.floor(12*Math.random());
-    color = "#ff"+Math.round(Math.random()*(1<<15));
-    console.log(color);
-    $('h2').show().html(text[ind]);
-    $('h2').css({"left":x-5+"px","top":y-5+"px","color":color}).fadeOut(1000);
-  });
-}
+    ind = Math.floor(texts.length*Math.random());
+    color = ""+Math.round(Math.random()*(1<<20));
+    var showtext = $('<span>').html(texts[ind]);
+    showtext.css({
+      "top":(y-15),
+      "left":x,
+      "position":"absolute",
+      "color":color,
+      "font-weight":"bold"
+    });
+    $(this).append(showtext);
+    showtext.animate(
+      {"top": y - 180,"opacity": 0},
+      1000,
+      function(){
+        this.remove();
+      });
+    });
+  }
